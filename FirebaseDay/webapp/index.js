e1aacce487881e13f182bfbac15f566d91e0c8ec
Todd Everett Griffin <goddtriffin@gmail.com>
2019-03-30 16:59:48 -0400

    Complete webapp message board.

--- FirebaseDay/webapp/index.js
+++ FirebaseDay/webapp/index.js
@@ -1,15 +1,70 @@
-document.getElementById("addMessage").addEventListener("click", () => {
-    addMessage("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut id orci porta, aliquet nulla non, ultricies quam. Donec ac venenatis odio. Aliquam non orci sed ante hendrerit condimentum. Donec sollicitudin diam ut metus ultrices fringilla. Vivamus at posuere justo. Morbi mollis pellentesque porta. Sed eget condimentum ipsum, ac eleifend mi. Phasellus auctor, nisi at dapibus fermentum, felis sem viverra felis, non molestie ipsum massa vel lectus. Cras imperdiet, urna semper fermentum facilisis, nibh libero faucibus sapien, sed placerat massa velit vel est.");
+// sign in anonymously
+firebase.auth().signInAnonymously().catch((error) => {
+  console.log('error signing in:', error);
 });
 
+// check for auth state change
+firebase.auth().onAuthStateChanged((user) => {
+  if (user) {
+    // signed in
+    console.log('signed in:', user);
+  } else {
+    // signed out
+    console.log('signed out');
+  }
+});
+
+// initialize the database connection
+const db = firebase.firestore();
+
+// add test data
+// db.collection("messages").add({
+//   content: 'Hello, World!',
+//   author: "Tester",
+//   timestamp: Date.now(),
+//   uid: (firebase.auth().currentUser !== null)? firebase.auth().currentUser.uid : 'aaaaa'
+// })
+// .then(function(docRef) {
+//   console.log("Document written with ID: ", docRef.id);
+// })
+// .catch(function(error) {
+//   console.error("Error adding document: ", error);
+// });
+
+// get live updates
+db.collection("messages").orderBy('timestamp', 'asc').onSnapshot(function(querySnapshot) {
+  clearAllMessages();
+  querySnapshot.forEach((doc) => {
+    addMessage(
+      ((!!doc.data().author)? doc.data().author  + ' ' : '') +
+      '(uid: ' + doc.data().uid.substring(0, 5) + '...): ' +
+      doc.data().content
+    );
+  });
+});
+
+// add a message to the board
 function addMessage(message) {
-    // create message p
-    const messageP = document.createElement("p");
+  // create message p
+  const messageP = document.createElement("p");
 
-    // set attributes
-    messageP.innerHTML = message;
-    messageP.classList.add('message');
+  // set attributes
+  messageP.innerHTML = message;
+  messageP.classList.add('message');
 
-    // put the message along with the others
-    document.getElementById("messages").appendChild(messageP);
+  // put the message along with the others
+  document.getElementById("messages").prepend(messageP);
 }
+
+// clear all messages from the board
+function clearAllMessages() {
+  const messages = document.getElementById("messages")
+  while (messages.firstChild) {
+    messages.removeChild(messages.firstChild);
+  }
+}
+
+// let counter = 5;
+// document.getElementById("addMessage").addEventListener("click", () => {
+//   addMessage(counter++);
+// });
